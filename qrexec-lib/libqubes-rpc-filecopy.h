d1238ce060fca3e5cb7cc85306ac8103d1a20131
Marek Marczykowski-Górecki <marmarek@invisiblethingslab.com>
2015-11-03 03:42:49 +0100

    qrexec-lib: add qfile packing functions to libqubes-rpc-filecopy
    This is counterpart of 5774c78 "qfile-agent: move data handling code to
    libqubes-rpc-filecopy" in core-agent-linux. The main purpose is to
    easily reuse the code in dom0 qvm-copy-to-vm tool.
    
    QubesOS/qubes-issues#1324

--- qrexec-lib/libqubes-rpc-filecopy.h
+++ qrexec-lib/libqubes-rpc-filecopy.h
@@ -30,6 +30,9 @@
 #define LEGAL_EOF 31415926
 
 #include <stdint.h>
+#include <stdarg.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 
 struct file_header {
 	unsigned int namelen;
@@ -60,8 +63,13 @@ enum {
 	COPY_FILE_WRITE_ERROR
 };
 
+/* feedback handling */
 typedef void (notify_progress_t)(int, int);
+typedef void (error_handler_t)(const char *fmt, va_list args);
 void register_notify_progress(notify_progress_t *func);
+void register_error_handler(error_handler_t *func);
+
+/* common functions */
 int copy_file(int outfd, int infd, long long size, unsigned long *crc32);
 const char *copy_file_status_to_str(int status);
 void set_size_limit(unsigned long long new_bytes_limit, unsigned long long new_files_limit);
@@ -74,9 +82,22 @@ int copy_fd_all(int fdout, int fdin);
 void set_nonblock(int fd);
 void set_block(int fd);
 
+/* unpacking */
 extern unsigned long Crc32_ComputeBuf( unsigned long inCrc32, const void *buf,
         size_t bufLen );
-
 extern int do_unpack(void);
+
+/* packing */
+int single_file_processor(const char *filename, const struct stat *st);
+int do_fs_walk(const char *file, int ignore_symlinks);
+/* used in tar2qfile to alter only headers, but keep original file stream */
+void write_headers(const struct file_header *hdr, const char *filename);
+int copy_file_with_crc(int outfd, int infd, long long size);
+/* MUST be called before first do_fs_walk/single_file_processor */
+void qfile_pack_init(void);
+void set_ignore_quota_error(int value);
+/* those two will call registered error handler if needed */
+void wait_for_result(void);
+void notify_end_and_wait_for_result(void);
 
 #endif /* _LIBQUBES_RPC_FILECOPY_H */
