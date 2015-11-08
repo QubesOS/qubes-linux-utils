25c05a3bd34956ad1a5fa4d904817eaee5614a54
Marek Marczykowski-Górecki <marmarek@invisiblethingslab.com>
2015-11-09 00:44:24 +0100

    dracut: add dmsetup --noudevsync since we're running in pre-udev hook
    When udev is installed (and scheduled to be started), but not running
    yet, plain dmsetup will deadlock on waiting for udev. Since we call
    `dmsetup mknodes dmroot` anyway
    
    QubesOS/qubes-issues#1354

--- dracut/full-dmroot/qubes_cow_setup.sh
+++ dracut/full-dmroot/qubes_cow_setup.sh
@@ -37,12 +37,12 @@ EOF
     while ! [ -e /dev/xvdc2 ]; do sleep 0.1; done
 
     echo "0 `cat /sys/block/xvda/size` snapshot /dev/xvda /dev/xvdc2 N 16" | \
-        dmsetup create dmroot || { echo "Qubes: FATAL: cannot create dmroot!"; }
+        dmsetup --noudevsync create dmroot || { echo "Qubes: FATAL: cannot create dmroot!"; }
     echo Qubes: done.
 else
     echo "Qubes: Doing R/W setup for TemplateVM..."
     echo "0 `cat /sys/block/xvda/size` linear /dev/xvda 0" | \
-        dmsetup create dmroot || { echo "Qubes: FATAL: cannot create dmroot!"; exit 1; }
+        dmsetup --noudevsync create dmroot || { echo "Qubes: FATAL: cannot create dmroot!"; exit 1; }
     echo Qubes: done.
 fi
 dmsetup mknodes dmroot
