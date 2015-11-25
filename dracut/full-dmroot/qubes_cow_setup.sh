a23030e49f20f5c46ee69dfb80821d4f3a4af45d
Marek Marczykowski-Górecki <marmarek@invisiblethingslab.com>
2015-11-25 03:41:04 +0100

    initramfs: initialize volatile.img partition table also in TemplateVM
    While there is no additional snapshot device, swap partition is still
    used.
    
    Fixes QubesOS/qubes-issues#1308

--- dracut/full-dmroot/qubes_cow_setup.sh
+++ dracut/full-dmroot/qubes_cow_setup.sh
@@ -83,6 +83,15 @@ EOF
     log_end
 else
     log_begin "Qubes: Doing R/W setup for TemplateVM..."
+    while ! [ -e /dev/xvdc ]; do sleep 0.1; done
+    sfdisk -q --unit S /dev/xvdc >/dev/null <<EOF
+1,$SWAP_SIZE,S
+EOF
+    if [ $? -ne 0 ]; then
+        die "Qubes: failed to setup partitions on volatile device"
+    fi
+    while ! [ -e /dev/xvdc1 ]; do sleep 0.1; done
+    mkswap /dev/xvdc1
     echo "0 `cat /sys/block/xvda/size` linear /dev/xvda 0" | \
         dmsetup --noudevsync create dmroot || die "Qubes: FATAL: cannot create dmroot!"
     log_end
