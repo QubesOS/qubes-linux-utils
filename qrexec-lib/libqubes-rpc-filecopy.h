af7ae9bd0cf72af29466d5d67f71e30d5a82ee7a
Demi Marie Obenour <demi@invisiblethingslab.com>
2024-05-12 10:29:33 -0400

    Add flag to allow non-canonical symbolic links
    This will later be exposed by qfile-unpacker, and is a safer alternative
    to disabling all symbolic link checking.

--- qrexec-lib/libqubes-rpc-filecopy.h
+++ qrexec-lib/libqubes-rpc-filecopy.h
@@ -68,6 +68,7 @@ enum copy_flags {
     COPY_ALLOW_SYMLINKS = (1 << 0),
     COPY_ALLOW_DIRECTORIES = (1 << 1),
     COPY_ALLOW_UNSAFE_CHARACTERS = (1 << 2),
+    COPY_ALLOW_NON_CANONICAL_SYMLINKS = (1 << 3),
 };
 
 /* feedback handling */
